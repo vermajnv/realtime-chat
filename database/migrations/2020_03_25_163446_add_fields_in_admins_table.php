2ad2dbc9e77b8d1561f03af9937345a2a0cbefb0
Nayan <nayanrahul.jnv@gmail.com>
2020-03-29 01:35:35 +0530

    Add fields to admin table

---
+++ database/migrations/2020_03_25_163446_add_fields_in_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsInAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->renameColumn('name', 'first_name');
+            $table->string('last_name')->after('name')->nullable();
+            $table->integer('mobile_number')->after('email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->renameColumn('first_name', 'name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('mobile_number');
+        });
+    }
+}
