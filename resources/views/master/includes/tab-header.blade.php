d9a2d4736d291a38301d3cbf88b51f643e831992
Nayan <nayanrahul.jnv@gmail.com>
2020-04-04 19:36:17 +0530

    reuse code

---
+++ resources/views/master/includes/tab-header.blade.php
@@ -0,0 +1,10 @@
+<div class="card-header p-0 pt-1">
+    <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill" href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home" aria-selected="true">English</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#custom-tabs-one-profile" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Hindi</a>
+        </li>
+    </ul>
+</div>
